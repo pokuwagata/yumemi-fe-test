1a70a5e24fae03155daec5808350324f281f889b
pokuwagata <pokuwagata@gmail.com>
2024-10-25 11:59:47 +0900

    env: 人口構成 API 疎通確認

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import useSWR from "swr";
+import useSWRMutation from "swr/mutation";
 
 async function fetcher(path: string) {
   const res = await fetch("https://opendata.resas-portal.go.jp/api" + path, {
@@ -11,14 +12,31 @@ async function fetcher(path: string) {
   return json;
 }
 
+async function populationFetcher(path: string, { arg }: { arg: string }) {
+  return await fetcher(`${path}?cityCode=-&prefCode=${arg}`);
+}
+
 export default function Home() {
   const { data, isLoading, error } = useSWR("/v1/prefectures", fetcher);
+  const { data: population, trigger } = useSWRMutation(
+    "/v1/population/composition/perYear",
+    populationFetcher,
+  );
 
   if (error) return <p>error: {JSON.stringify(error)}</p>;
   if (isLoading) return <p>isLoading</p>;
 
   return (
     <>
+      <button
+        onClick={async () => {
+          await trigger("1");
+        }}
+      >
+        人口構成取得
+      </button>
+      <p>population:</p>
+      <p>{population ? JSON.stringify(population) : null}</p>
       <p>data:</p>
       <p>{JSON.stringify(data)}</p>
     </>
