85c12d574b836637c064cb1bba8eb522c49c09e9
pokuwagata <pokuwagata@gmail.com>
2024-10-28 11:10:06 +0900

    fix: CI でのみ発生する型エラーの調査

--- src/features/home/components/Home/Home.tsx
+++ src/features/home/components/Home/Home.tsx
@@ -1,4 +1,4 @@
-import styles from "./Home.module.css";
+// import styles from "./Home.module.css";
 
 import { Chart } from "~/features/home/components/Chart";
 import { Header } from "~/features/home/components/Header/Header";
@@ -6,13 +6,16 @@ import { Prefectures } from "~/features/home/components/Prefectures";
 
 export function Home() {
   return (
-    <div className={styles.layout}>
-      <div className={styles.header}>
+    // <div className={styles.layout}>
+    //   <div className={styles.header}>
+    <div>
+      <div>
         <Header>ゆめみフロントエンドコーディング試験</Header>
       </div>
       <main>
         <section>
-          <div className={styles.prefectures}>
+          {/* <div className={styles.prefectures}> */}
+          <div>
             <Prefectures />
           </div>
           <Chart />
