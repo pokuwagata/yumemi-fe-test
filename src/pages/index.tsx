16bcafa0f549413930e7cacf5d99d2fd6a869779
pokuwagata <pokuwagata@gmail.com>
2024-10-28 10:56:49 +0900

    feat: メタタグ設定

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,18 @@
+import Head from "next/head";
+
 import { Home } from "~/features/home/components/Home";
 
 export default function Page() {
-  return <Home />;
+  return (
+    <>
+      <Head>
+        <title>ゆめみフロントエンドコーディング試験</title>
+        <meta
+          name="description"
+          content="ゆめみフロントエンドコーディング試験"
+        />
+      </Head>
+      <Home />
+    </>
+  );
 }
