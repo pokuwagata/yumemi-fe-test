342d750ee844ad3908b4ca9b0dbbb151e1e788aa
pokuwagata <pokuwagata@gmail.com>
2024-10-28 11:20:21 +0900

    fix: next-env.d.ts をコミット（GitHub Actions でのみ型エラーが発生する）

--- src/features/home/components/Home/Home.tsx
+++ src/features/home/components/Home/Home.tsx
@@ -1,4 +1,4 @@
-// import styles from "./Home.module.css";
+import styles from "./Home.module.css";
 
 import { Chart } from "~/features/home/components/Chart";
 import { Header } from "~/features/home/components/Header/Header";
@@ -6,16 +6,13 @@ import { Prefectures } from "~/features/home/components/Prefectures";
 
 export function Home() {
   return (
-    // <div className={styles.layout}>
-    //   <div className={styles.header}>
-    <div>
-      <div>
+    <div className={styles.layout}>
+      <div className={styles.header}>
         <Header>ゆめみフロントエンドコーディング試験</Header>
       </div>
       <main>
         <section>
-          {/* <div className={styles.prefectures}> */}
-          <div>
+          <div className={styles.prefectures}>
             <Prefectures />
           </div>
           <Chart />
