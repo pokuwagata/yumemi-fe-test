aac86490f7918fb3e68d9b87200128e5b750234f
pokuwagata <pokuwagata@gmail.com>
2024-10-28 17:25:46 +0900

    feat: 整形したデータからグラフを描画する

--- src/features/home/components/Chart/Chart.tsx
+++ src/features/home/components/Chart/Chart.tsx
@@ -1,15 +1,26 @@
 import { useEffect, useState } from "react";
+import {
+  CartesianGrid,
+  Label,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 import { getPopulationData } from "~/features/home/lib/getPopulationData";
 import { RawPopulationResponses } from "~/features/home/types/api";
 import { RechartsDataItem } from "~/features/home/types/recharts";
 import { requestPopulation } from "~/lib/requestPopulation";
+import { PrefecturesResponse } from "~/types/api";
 
 type Props = {
   codes: number[];
+  prefectures: PrefecturesResponse["result"];
 };
 
-export function Chart({ codes }: Props) {
+export function Chart({ codes, prefectures }: Props) {
   const [data, setData] = useState<RechartsDataItem[]>([]);
   const [type, setType] = useState(1);
 
@@ -25,13 +36,38 @@ export function Chart({ codes }: Props) {
     }
 
     fetchData();
-  }, [codes]);
-
-  console.log(data);
+  }, [codes, type]);
 
   return (
     <figure>
-      <p>グラフ</p>
+      <LineChart
+        width={800}
+        height={800}
+        data={data}
+        margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
+      >
+        <XAxis dataKey="name">
+          <Label value="年度" position="bottom" />
+        </XAxis>
+        <YAxis tickFormatter={(value) => (value / 10e4).toFixed(1)}>
+          <Label value={"人口数 (万人)"} angle={-90} position="left" />
+        </YAxis>
+        {codes.map((code) => {
+          const name = prefectures.find((p) => p.prefCode === code)?.prefName;
+
+          return (
+            <Line
+              type="monotone"
+              name={name}
+              dataKey={code}
+              stroke="#8884d8"
+              key={code}
+            />
+          );
+        })}
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <Tooltip />
+      </LineChart>
     </figure>
   );
 }
