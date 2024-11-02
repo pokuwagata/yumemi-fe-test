1da301cf3e20b056c61cf5ab1012ce7fa71095ee
pokuwagata <pokuwagata@gmail.com>
2024-11-02 23:37:04 +0900

    refactor: Recharts のグラフを別コンポーネントに切り出す

--- src/features/home/components/ChartContainer/ChartContainer.tsx
+++ src/features/home/components/ChartContainer/ChartContainer.tsx
@@ -1,17 +1,7 @@
 import { useEffect, useState } from "react";
-import {
-  CartesianGrid,
-  Label,
-  Line,
-  LineChart,
-  ResponsiveContainer,
-  Tooltip,
-  XAxis,
-  YAxis,
-} from "recharts";
 
+import { ChartPresenter } from "~/features/home/components/ChartPresenter/ChartPresenter";
 import { PopulationLabels } from "~/features/home/components/PopulationLabels/PopulationLabels";
-import { usePrefecturesContext } from "~/features/home/contexts/PrefecturesContext";
 import { useSelectedPrefCodesContext } from "~/features/home/contexts/SelectedPrefCodesContext";
 import { getPopulationData } from "~/features/home/lib/getPopulationData";
 import {
@@ -22,7 +12,6 @@ import { RechartsDataItem } from "~/features/home/types/recharts";
 import { requestPopulation } from "~/lib/requestPopulation";
 
 export function ChartContainer() {
-  const prefectures = usePrefecturesContext();
   const { codes } = useSelectedPrefCodesContext();
 
   const [data, setData] = useState<RechartsDataItem[]>([]);
@@ -56,38 +45,7 @@ export function ChartContainer() {
         <p data-testid="caution-text">都道府県を選択してください</p>
       ) : (
         <>
-          <figure>
-            <ResponsiveContainer aspect={0.5} maxHeight={500}>
-              <LineChart
-                data={data}
-                margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
-              >
-                <XAxis dataKey="name">
-                  <Label value="年度" position="bottom" />
-                </XAxis>
-                <YAxis tickFormatter={(value) => (value / 10e4).toFixed(1)}>
-                  <Label value={"人口数 (万人)"} angle={-90} position="left" />
-                </YAxis>
-                {codes.map((code) => {
-                  const name = prefectures.find(
-                    (p) => p.prefCode === code,
-                  )?.prefName;
-
-                  return (
-                    <Line
-                      type="monotone"
-                      name={name}
-                      dataKey={code}
-                      stroke="#8884d8"
-                      key={code}
-                    />
-                  );
-                })}
-                <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                <Tooltip />
-              </LineChart>
-            </ResponsiveContainer>
-          </figure>
+          <ChartPresenter data={data} />
           {isLoading && <p>Loading</p>}
         </>
       )}
---
+++ src/features/home/components/ChartPresenter/ChartPresenter.tsx
@@ -0,0 +1,56 @@
+import {
+  CartesianGrid,
+  Label,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+import { usePrefecturesContext } from "~/features/home/contexts/PrefecturesContext";
+import { useSelectedPrefCodesContext } from "~/features/home/contexts/SelectedPrefCodesContext";
+import { RechartsDataItem } from "~/features/home/types/recharts";
+
+type Props = {
+  data: RechartsDataItem[];
+};
+
+export function ChartPresenter({ data }: Props) {
+  const prefectures = usePrefecturesContext();
+  const { codes } = useSelectedPrefCodesContext();
+
+  return (
+    <figure>
+      <ResponsiveContainer aspect={0.5} maxHeight={500}>
+        <LineChart
+          data={data}
+          margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
+        >
+          <XAxis dataKey="name">
+            <Label value="年度" position="bottom" />
+          </XAxis>
+          <YAxis tickFormatter={(value) => (value / 10e4).toFixed(1)}>
+            <Label value={"人口数 (万人)"} angle={-90} position="left" />
+          </YAxis>
+          {codes.map((code) => {
+            const name = prefectures.find((p) => p.prefCode === code)?.prefName;
+
+            return (
+              <Line
+                type="monotone"
+                name={name}
+                dataKey={code}
+                stroke="#8884d8"
+                key={code}
+              />
+            );
+          })}
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <Tooltip />
+        </LineChart>
+      </ResponsiveContainer>
+    </figure>
+  );
+}
