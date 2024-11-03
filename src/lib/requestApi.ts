86af43c75fa67e797385989e341f958967013388
pokuwagata <pokuwagata@gmail.com>
2024-11-03 11:33:14 +0900

    feat: API エラーレスポンスが 400 系の場合のエラーハンドリング

--- src/lib/requestApi.ts
+++ src/lib/requestApi.ts
@@ -1,5 +1,12 @@
 import { API_BASE_URL } from "~/lib/const";
 
+const errorCodeToMessage: { [key in string]: string } = {
+  "400": "Bad Request",
+  "403": "Forbidden",
+  "404": "NotFound",
+  "429": "Too Many Requests",
+};
+
 export async function requestApi<T>(path: string): Promise<T> {
   try {
     const res = await fetch(`${API_BASE_URL}${path}`, {
@@ -9,8 +16,20 @@ export async function requestApi<T>(path: string): Promise<T> {
     });
     const json = await res.json();
 
-    if (typeof json === "string") {
-      if (json === "400") throw new Error("400 Bad Request");
+    if (typeof json === "string" && errorCodeToMessage.hasOwnProperty(json)) {
+      // エラーコード 400, 404 で body が string の場合
+      throw new Error(`${json} ${errorCodeToMessage[json]}`);
+    } else if (typeof json === "object") {
+      if (json.hasOwnProperty("statusCode")) {
+        // エラーコード 403, 404
+        throw new Error(
+          `${json.statusCode} ${errorCodeToMessage[json.statusCode]}`,
+        );
+      }
+
+      if (res.status === 429) {
+        throw new Error(`429 ${errorCodeToMessage["429"]}`);
+      }
     }
 
     return json;
